ac17256f3a15b022474e7fe2ce25e89ef435c86c
HafizyahRayhan <hafizyah.rayhan@ui.ac.id>
2023-11-17 22:14:05 +0700

    CS08 - Menyelesaikan CS 8

---
+++ app/src/main/java/com/HafizyahRayhanZulikhramJBusBR/jbus_android/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.HafizyahRayhanZulikhramJBusBR.jbus_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.*;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView registerNow=null;
+    private Button loginButton =null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        registerNow = findViewById(R.id.register_now);
+        loginButton = findViewById(R.id.login_button);
+        registerNow.setOnClickListener( view -> {moveActivity(this, RegisterActivity.class);});
+        loginButton.setOnClickListener( view -> {moveActivity(this, MainActivity.class);});
+        getSupportActionBar().hide();
+    }
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent (ctx,cls);
+        startActivity(intent);
+    }
+    private void viewToast(Context ctx, String message){
+        Toast.makeText(ctx,message,Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/HafizyahRayhanZulikhramJBusBR/jbus_android/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.HafizyahRayhanZulikhramJBusBR.jbus_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.*;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+    }
+}
